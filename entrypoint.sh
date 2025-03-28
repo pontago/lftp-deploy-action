ff33ba6da04b21e0b452651e9737b5c29bdee4ba
pontago <pontagoo@gmail.com>
2025-03-28 22:13:40 +0900

    Action実装。

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/sh -l
 
-#set -eux
+set -eu
+
+# Check if the debug mode is enabled
+if [ "$INPUT_DEBUG" = true ]; then
+	LFTP_DEBUG="debug 3"
+	LFTP_MIRROR_VERBOSE="--verbose"
+else 
+	LFTP_DEBUG=""
+	LFTP_MIRROR_VERBOSE=""
+fi
+
+# Check if the dry run mode is enabled
+if [ "$INPUT_DRY_RUN" = true ]; then
+	LFTP_DRY_RUN="--dry-run --just-print"
+else 
+	LFTP_DRY_RUN=""
+fi
 
 # Check if the required inputs are set
 if [ "$INPUT_HOST" = "" ] && [ "$INPUT_USERNAME" = "" ]; then
@@ -8,24 +24,22 @@ if [ "$INPUT_HOST" = "" ] && [ "$INPUT_USERNAME" = "" ]; then
 	exit 1
 fi
 
-if [ "$INPUT_LOCAL_DIR" = "" ] && [ "$INPUT_REMOTE_DIR" = "" ]; then
-	if [ "$INPUT_SCRIPT" = "" ]; then
-		echo "Local directory, remote directory, and lftp script is not set. Exiting..."
-		exit 1
-	else
-		LFTP_SCRIPT=$INPUT_SCRIPT
-	fi
-	LFTP_LOCAL_DIR=""
-	LFTP_REMOTE_DIR=""
+if [ "$INPUT_LOCAL_DIR" = "" ] && [ "$INPUT_REMOTE_DIR" = "" ] && [ "$INPUT_SCRIPT" = "" ]; then
+	echo "Local directory, remote directory, and lftp script is not set. Exiting..."
+	exit 1
+elif [ "$INPUT_LOCAL_DIR" != "" ] && [ "$INPUT_REMOTE_DIR" != "" ]; then
+	LFTP_MIRROR="mirror $LFTP_MIRROR_VERBOSE --reverse --only-newer $LFTP_DRY_RUN $INPUT_LOCAL_DIR $INPUT_REMOTE_DIR"
 else
-	LFTP_LOCAL_DIR=$INPUT_LOCAL_DIR
-	LFTP_REMOTE_DIR=$INPUT_REMOTE_DIR
-	if [ "$INPUT_SCRIPT" != "" ]; then
-		LFTP_SCRIPT=$INPUT_SCRIPT
-	fi
+	LFTP_MIRROR=""
+fi
+
+# Check if the lftp script is set
+if [ "$INPUT_SCRIPT" != "" ]; then
+	LFTP_SCRIPT=$INPUT_SCRIPT
 fi
 
 # Check ftp protocol
+LFTP_PRIVATE_KEY=""
 if [ "$INPUT_PROTOCOL" = "ftp" ]; then
 	LFTP_PROTOCOL="ftp"
 	LFTP_SSL="set ftp:ssl-allow no"
@@ -41,55 +55,70 @@ set ftp:ssl-protect-fxp true
 "
 elif [ "$INPUT_PROTOCOL" = "sftp" ]; then
 	LFTP_PROTOCOL="sftp"
-	LFTP_SSL="
-set ftp:ssl-allow no
-set sftp:auto-confirm no
-"
+	LFTP_SSL="set sftp:auto-confirm yes"
+
+	SSH_PRIVATE_KEY_FILE='../ssh-private-key'
+	echo "$INPUT_SSH_PRIVATE_KEY" >$SSH_PRIVATE_KEY_FILE
+	chmod 600 $SSH_PRIVATE_KEY_FILE
+	LFTP_PRIVATE_KEY="set sftp:connect-program 'ssh -a -x -o StrictHostKeyChecking=no -i $SSH_PRIVATE_KEY_FILE'"
 else
 	echo "Protocol is not set. Exiting..."
 	exit 1
 fi
 
-
-# Check if the debug mode is enabled
-if [ "$INPUT_DEBUG" = true ]; then
-	LFTP_DEBUG="debug 9"
-else 
-	LFTP_DEBUG=""
+# Check if the port is set
+if [ "$INPUT_PORT" = "" ]; then
+	LFTP_PORT=""
+else
+	LFTP_PORT="-p $INPUT_PORT"
 fi
 
-# Check if the dry run mode is enabled
-if [ "$INPUT_DRY_RUN" = true ]; then
-	LFTP_DRY_RUN="--dry-run --just-print"
-else 
-	LFTP_DRY_RUN=""
+if [ "$INPUT_VERIFY_CERT" = true ]; then
+	LFTP_VERIFY_CERT="set ssl:verify-certificate yes"
+else
+	LFTP_VERIFY_CERT="set ssl:verify-certificate no"
+fi
+
+if [ "$INPUT_CREATE_REMOTE_DIR" = true ] && [ "$INPUT_REMOTE_DIR" != "" ]; then
+	if [ "$INPUT_DRY_RUN" = true ]; then
+		LFTP_CREATE_REMOTE_DIR="!echo 'mkdir -p -f $INPUT_REMOTE_DIR'"
+	else
+		LFTP_CREATE_REMOTE_DIR="mkdir -p -f $INPUT_REMOTE_DIR"
+	fi
+else
+	LFTP_CREATE_REMOTE_DIR=""
+fi
+
+# Check if the timeout is set
+if [ "$INPUT_TIMEOUT" = "" ]; then
+	LFTP_TIMEOUT=""
+else
+	LFTP_TIMEOUT="set net:timeout $INPUT_TIMEOUT"
+fi
+
+if [ "$INPUT_MAX_RETRIES" = "" ]; then
+	LFTP_MAX_RETRIES=""
+else
+	LFTP_MAX_RETRIES="set net:max-retries $INPUT_MAX_RETRIES"
 fi
 
 echo 'Uploading files to server...'
 
-# LFTP_SCRIPT=''
-# echo 'TEST' >> $LFTP_SCRIPT
-
-# echo $LFTP_SCRIPT
-
-#set sftp:connect-program 'ssh -a -x -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE'
-#open sftp://$FTP_USERNAME:dummy@$FTP_SERVER
-
-ssh -a -x -o StrictHostKeyChecking=no $INPUT_USERNAME@$INPUT_HOST touch /public_html/test.txt
-
-# lftp <<EOS
-# # set cmd:fail-exit true
-# # set xfer:log 1
-# $LFTP_DEBUG
-# #$LFTP_SSL
-# set sftp:connect-program 'ssh -a -x -o StrictHostKeyChecking=no'
-# open -u $INPUT_USERNAME,$INPUT_PASSWORD $LFTP_PROTOCOL://$INPUT_HOST
-# #user $INPUT_USERNAME $INPUT_PASSWORD
-# #mirror --reverse --overwrite $LFTP_DRY_RUN $LFTP_LOCAL_DIR $LFTP_REMOTE_DIR
-# $LFTP_SCRIPT
-# #close
-# #exit
-# EOS
+lftp <<EOS
+set cmd:fail-exit true
+set xfer:log 1
+$LFTP_TIMEOUT
+$LFTP_MAX_RETRIES
+$LFTP_DEBUG
+$LFTP_SSL
+$LFTP_VERIFY_CERT
+$LFTP_PRIVATE_KEY
+open -u $INPUT_USERNAME,$INPUT_PASSWORD $LFTP_PORT $LFTP_PROTOCOL://$INPUT_HOST
+$LFTP_CREATE_REMOTE_DIR
+$LFTP_MIRROR
+$LFTP_SCRIPT
+bye
+EOS
 
 echo 'Files uploaded successfully!'
 
